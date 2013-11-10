f2e19a60810201cf6664f3d725a43702ba9f989c
James Sofra <James.Sofra@gmail.com>
2013-11-11 00:23:27 +1100

    Update clojure and geotools versions.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject meridian/georeference "0.0.1-SNAPSHOT"
+(defproject meridian/georeference "0.0.2-SNAPSHOT"
   :description "georeference is a library for defining and interacting with geospatial coordinate reference systems (CRS) in Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.geotools/gt-referencing "2.7.4"]
-                 [org.geotools/gt-epsg-hsql "2.7.4"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [instaparse "1.1.0"]
+                 [org.geotools/gt-referencing "9.3"]
+                 [org.geotools/gt-epsg-hsql "9.3"]]
   :repositories {"osgeo.org" "http://download.osgeo.org/webdav/geotools/"})
