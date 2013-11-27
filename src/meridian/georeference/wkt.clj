9e45dd51d7627e3fde0042c0409a1df41285a07e
James Sofra <James.Sofra@gmail.com>
2013-11-28 00:25:09 +1100

    Remove multimethod wkt parsing and update instaparse version

--- src/meridian/georeference/wkt.clj
+++ src/meridian/georeference/wkt.clj
@@ -1,95 +1,6 @@
 (ns meridian.georeference.wkt
   (:require [clojure.string :as str]
-            [clojure.walk :as walk]
             [instaparse.core :as insta]))
-
-(defn WKT->clj-str [WKT]
-  (letfn [(keywordize [match] (str "[:" (apply str (butlast match)) " "))]
-   (str (str/replace WKT #"[A-Z][A-Z0-9_]*\[" keywordize) "]")))
-
-(defn WKT->clj-vec [WKT]
-  (read-string (WKT->clj-str WKT)))
-
-(defmulti vec-node-transform first)
-(defmethod vec-node-transform :SPHEROID
-  [[key name semi-major inverse-flattening authority]]
-  {key (merge {:NAME name :SEMI-MAJOR semi-major
-               :INVERSE-FLATTENING inverse-flattening} authority)})
-(defmethod vec-node-transform :AUTHORITY
-[[key name code]] {key (str name ":" code)})
-(defmethod vec-node-transform :PARAMETER
-  [[key name value]] {key {(keyword name) value}})
-(defmethod vec-node-transform :UNIT
-  [[key name factor]] {key [name factor]})
-(defmethod vec-node-transform :AXIS
-  [[key name axis]] {key {:NAME name :AXIS (keyword axis)}})
-(defmethod vec-node-transform :PROJECTION
-  [[key name authority]] {key (merge authority {:NAME name})})
-(defmethod vec-node-transform :PRIMEM
-  [[key name longitude authority]]
-  {key (merge {:NAME name :LONGITUDE longitude} authority)})
-(defmethod vec-node-transform :TOWGS84
-  [[key & params]] {key (into [] params)})
-(defmethod vec-node-transform :default
-  [[key name & r]]
-  (let [build-map-coll
-        (fn [maps k coll-k combine-fn]
-          (let [items (for [x maps :when (k x)] (k x))]
-            (if (empty? items) {} {coll-k (combine-fn items)})))
-        axes-map (build-map-coll r :AXIS :AXES #(into [] %))
-        param-map (build-map-coll r :PARAMETER :PARAMS #(apply merge %))
-        remaining (->> r (remove :AXIS) (remove :PARAMETER))]
-    {key (merge {:NAME name} axes-map param-map (apply merge remaining))}))
-
-(defn clj-vec->clj-map [clj-vec-wkt]
-  (walk/postwalk
-   (fn [x] (if (vector? x) (vec-node-transform x) x)) clj-vec-wkt))
-
-(defn WKT->clj-map [WKT]
-  (-> WKT WKT->clj-vec clj-vec->clj-map))
-
-(defn- str-vec-args [args & {:keys [sep open close] :or {sep ", " open "[" close "]"}}]
-  (str open (apply str (interpose sep args)) close))
-
-(defn- str-quote [s] (str "\"" s "\""))
-
-(defmulti map-node-trans (fn [k m]  k))
-
-(defn map-node-trans-props [k m & ks]
- (let [props (map #(map-node-trans % (k m)) ks)]
-    (str (name k) (str-vec-args props))))
-
-(defmethod map-node-trans :AUTHORITY [k m]
-  (str (name k) (str-vec-args (map str-quote (str/split (k m) #":")) :sep ",")))
-(defmethod map-node-trans :PARAMS [k m]
-  (let [param-strs (for [[key val] (seq (k m))]
-                     (str "PARAMETER" (str-vec-args [(str-quote (name key)) val])))]
-    (apply str (interpose ", \n" param-strs))))
-(defmethod map-node-trans :UNIT [k m]
-  (let [[n f] (k m)] (str (name k) (str-vec-args [(str-quote n) f]))))
-(defmethod map-node-trans :NAME [k m] (str-quote (k m)))
-(defmethod map-node-trans :AXES [k m]
-  (apply str
-         (interpose ", \n"
-                    (for [{:keys [NAME AXIS]} (k m)]
-                      (str "AXIS" (str-vec-args [(str-quote NAME) (symbol (name AXIS))]))))))
-(defmethod map-node-trans :TOWGS84 [k m] (str (name k) (str-vec-args (k m))))
-(defmethod map-node-trans :PROJECTION [k m]
-  (map-node-trans-props k m :NAME :AUTHORITY))
-(defmethod map-node-trans :SPHEROID [k m]
-  (map-node-trans-props k m :NAME :SEMI-MAJOR :INVERSE-FLATTENING :AUTHORITY))
-(defmethod map-node-trans :DATUM [k m]
-  (map-node-trans-props k m :NAME :SPHEROID :TOWGS84 :AUTHORITY))
-(defmethod map-node-trans :PRIMEM [k m]
-  (map-node-trans-props k m :NAME :LONGITUDE :AUTHORITY))
-(defmethod map-node-trans :GEOGCS [k m]
-  (map-node-trans-props k m :NAME :DATUM :PRIMEM :UNIT :AUTHORITY))
-(defmethod map-node-trans :PROJCS [k m]
-  (map-node-trans-props k m :NAME :GEOGCS :PROJECTION :PARAMS :UNIT :AXES :AUTHORITY))
-(defmethod map-node-trans :default [k m] (str (k m)))
-
-(defn clj-map->WKT [clj-map-wkt]
-  (map-node-trans :PROJCS clj-map-wkt))
 
 (def common-grammer-defs "
 open             = '[' | '('
@@ -116,7 +27,7 @@ passthrough-mt    = <'PASSTHROUGH_MT'> <open> integer <comma> math-transform <cl
 
 (def crs-grammer "
 <coordinate-system> = horz-cs | geocentric-cs | vert-cs | compd-cs | fitted-cs | local-cs
-horz-cs             = geographic-cs | projected-cs
+<horz-cs>             = geographic-cs | projected-cs
 projected-cs        = <'PROJCS'> <open> name <comma> geographic-cs <comma> projection <comma>
                       parameters <comma?> linear-unit (<comma> twin-axes)? (<comma> authority)? <close>
 projection          = <'PROJECTION'> <open> name (<comma> authority)? <close>
@@ -125,8 +36,8 @@ geographic-cs       = <'GEOGCS'> <open> name <comma> datum <comma> prime-meridia
 datum               = <'DATUM'> <open> name <comma> spheroid (<comma> to-wgs84)? (<comma> authority)? <close>
 spheroid            = <'SPHEROID'> <open> name <comma> semi-major-axis <comma>
                       inverse-flattening (<comma> authority)? <close>
-semi-major-axis     = number
-inverse-flattening  = number
+<semi-major-axis>   = number
+<inverse-flattening>= number
 prime-meridian      = <'PRIMEM'> <open> name <comma> longitude (<comma> authority)? <close>
 longitude           = number
 <angular-unit>      = unit
@@ -172,6 +83,13 @@ local-datum         = <'LOCAL_DATUM'> <open> name <comma> datum-type (<comma> au
   (insta/parser (str math-transform-grammer
                      common-grammer-defs)))
 
+(defn transform-fn [req opt]
+  (fn [& vals]
+    (zipmap req vals)
+    ))
+
+
+
 (defrecord ParamMT [name params])
 (defrecord ConcatMT [math-transforms])
 (defrecord InvMT [math-transform])
@@ -199,10 +117,16 @@ local-datum         = <'LOCAL_DATUM'> <open> name <comma> datum-type (<comma> au
 (defrecord Authority [name code])
 (defrecord Axis [name dimension])
 (defrecord Unit [name conversion-factor authority])
+(defrecord Datum [name spheroid towgs84 authority])
+(defrecord Spheroid [name semi-major-axis inverse-flattening authority])
 
 (def crs-transforms {:authority ->Authority
                      :axis ->Axis
-                     :unit ->Unit})
+                     :unit ->Unit
+                     :spheroid ->Spheroid
+                     ;:datum ->Datum
+                     ;:geographic-cs ->GeographicCS
+                     })
 
 (defn parse-crs [s]
   (insta/transform
